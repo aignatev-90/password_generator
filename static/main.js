9633aedf5bf1a6d21951336f6df90e9157fa57d3
maxmih <maxmih@rtr.spb.ru>
2023-05-11 13:29:23 +0300

    add styles for pswd generator

--- static/main.js
+++ static/main.js
@@ -1,38 +1,53 @@
-$( document ).ready(function() {
-    var checked = $('#specs').prop('checked')
+$(document).ready(function () {
+  var checked = $("#specs").prop("checked");
 
-    $('#pwd_length').keyup(function () {
-        var pwd_length = $('#pwd_length').val()
-        if(pwd_length > 20) {$('#pwd').text('Максимальная длинна пароля - 20 символов')}
-        else {makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length)};
-        return false;
+  $("#pwd_length").keyup(function () {
+    var pwd_length = $("#pwd_length").val();
+    if (pwd_length == 0 || pwd_length > 20){
+        $(".img_copy").css("display", "none");
+    } else {
+        $(".img_copy").css("display", "flex"); 
+    }
+    if (pwd_length > 20) {
+      $("#pwd").text("");
+    } else {
+      makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length);
+    }
+    return false;
+  });
+
+  $("#specs").change(function () {
+    var pwd_length = $("#pwd_length").val();
+    if (!checked) {
+      if (pwd_length > 20) {
+        $("#pwd").text("Максимальная длинна пароля - 20 символов");
+      } else {
+        makeAjaxRequest(
+          "retrieve_pwd/?pwd_length=" + pwd_length + "&specs=True"
+        );
+      }
+      checked = true;
+    } else {
+      makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length);
+      checked = false;
+    }
+    return false;
+  });
+
+  function makeAjaxRequest(url) {
+    $.ajax({
+      url: url,
+      success: function (response) {
+        response: $("#pwd").text(response.pwd);
+      },
+      else: function (response) {
+        console.log(response.responseJSON.errors);
+      },
     });
+  }
 
-
-    $('#specs').change(function () {
-        var pwd_length = $('#pwd_length').val()
-        if(!checked) {
-            if(pwd_length > 20) {$('#pwd').text('Максимальная длинна пароля - 20 символов')}
-            else {makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length + '&specs=True')};
-            checked = true;
-        }
-        else {
-            makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length);
-            checked = false;
-            };
-        return false;
-    });
-
-
-    function makeAjaxRequest(url)  {
-        $.ajax({
-            url: url,
-            success: function (response) {
-                response: $('#pwd').text(response.pwd)
-                },
-            else: function (response) {
-                console.log(response.responseJSON.errors)
-                }
-        });
-    };
+  $(".img_copy").on("click",function () {
+    var pwd = $(".show_gen_pswd").html();
+    navigator.clipboard.writeText(pwd)
+  })
 });
