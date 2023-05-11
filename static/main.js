797f26b46180c4dfa37f2e35c14b8214c70003e9
SugarDonut <samyray2009@yandex.ru>
2023-05-11 17:31:47 +0300

    add onload page generate pswd

--- static/main.js
+++ static/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
   var checked = $("#specs").prop("checked");
 
+  var pwd_length = $("#pwd_length").val();
+  copy_link_pwd_check(pwd_length)
+
+  makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length);
+
   $("#pwd_length").keyup(function () {
-    
     var pwd_length = $("#pwd_length").val();
-    if (pwd_length == 0 || pwd_length > 20){
-        $(".img_copy").css("display", "none");
-    } else {
-        $(".img_copy").css("display", "flex"); 
-    }
+    copy_link_pwd_check(pwd_length)
     if (pwd_length > 20) {
       $(".pwd_hint").text("Максимальная длинна пароля - 20 символов");
     } else {
@@ -48,8 +48,16 @@ $(document).ready(function () {
     });
   }
 
-  $(".img_copy").on("click",function () {
+  $(".img_copy").on("click", function () {
     var pwd = $(".show_gen_pswd").html();
-    navigator.clipboard.writeText(pwd)
-  })
+    navigator.clipboard.writeText(pwd);
+  });
+
+  function copy_link_pwd_check(pwd_length) {
+    if (pwd_length == 0 || pwd_length > 20) {
+      $(".img_copy").css("display", "none");
+    } else {
+      $(".img_copy").css("display", "flex");
+    }
+  }
 });
