dd60aa54d38c0a92ad5cf6850466edecf8cefb96
Andrey <aignatev-90@mail.ru>
2023-05-11 19:37:19 +0300

    fixed pwd length issue

--- static/main.js
+++ static/main.js
@@ -20,18 +20,23 @@ $(document).ready(function () {
     var checked = $("#specs").prop("checked");
 
     copy_link_pwd_check(pwd_length);
-    if (pwd_length >= 20 || pwd_length < 0) {
-      $("#pwd_length").val(20);
-      pwd_length = 8;
+    if (pwd_length > 20) {
       $(".pwd_hint").text("Максимальная длина пароля - 20 символов");
-    } else {
-      $(".pwd_hint").text("");
-      if (!checked) {
-        makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length);
-      } else {
-        makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length + "&specs=True");
-      }
+      pwd_length = 20
     }
+     else if (pwd_length < 8) {
+      $(".pwd_hint").text("Минимальная длина пароля - 8 символов");
+      pwd_length = 8
+    }
+     else {
+      $(".pwd_hint").text("");
+      }
+  if (!checked) {
+    makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length);
+  } else {
+    makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length + "&specs=True");
+  }
+
     });
     if (!checked || pwd_length > 20) {
       makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length);
