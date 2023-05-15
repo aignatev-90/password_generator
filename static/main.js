f69114b9cab4b16fb67ee165b3d6085f8f90ca6c
Andrey <aignatev-90@mail.ru>
2023-05-15 12:27:18 +0300

    fixed length bugs

--- static/main.js
+++ static/main.js
@@ -1,57 +1,35 @@
 $(document).ready(function () {
   var checked = $("#specs").prop("checked");
-  var pwd_length = $("#pwd_length").val();
+  var pwd_length = correct_pwd_length($("#pwd_length").val());
 
   makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length);
 
   $(".img_copy").show();
 
-  if (pwd_length > 20) {
-    $(".pwd_hint").text("Максимальная длина пароля - 20 символов");
-    pwd_length = 20;
-  } else if (pwd_length < 8) {
-    $(".pwd_hint").text("Минимальная длина пароля - 8 символов");
-    pwd_length = 8;
-  } else {
-    $(".pwd_hint").text("");
-  }
   if (!checked) {
     makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length);
   } else {
     makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length + "&specs=True");
   }
 
-  $("#pwd_length").keyup(function () {
-    var pwd_length = $("#pwd_length").val();
+  $("#pwd_length").on("change", function () {
+    var pwd_length = correct_pwd_length($("#pwd_length").val());
     var checked = $("#specs").prop("checked");
 
-    if (pwd_length > 20) {
-      $(".pwd_hint").text("Максимальная длина пароля - 20 символов");
-      pwd_length = 20;
-    } else if (pwd_length < 8) {
-      $(".pwd_hint").text("Минимальная длина пароля - 8 символов");
-      pwd_length = 8;
-    } else {
-      $(".pwd_hint").text("");
-    }
     if (!checked) {
       makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length);
     } else {
       makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length + "&specs=True");
     }
   });
-  if (!checked || pwd_length > 20) {
-    makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length);
-  } else {
-    makeAjaxRequest("retrieve_pwd/?pwd_length=" + pwd_length + "&specs=True");
-  }
 
   /*
    * START Обработка событий при нажатии увеличения и уменшения значений пароля
    */
 
   $(".plus").click(function () {
-    var pwd_length = $("#pwd_length").val();
+    var pwd_length = correct_pwd_length($("#pwd_length").val());
+    console.log('pwd_length', pwd_length)
     var checked = $("#specs").prop("checked");
     this.parentNode.querySelector("input[type=number]").stepUp();
     if (!checked) {
@@ -63,7 +41,7 @@ $(document).ready(function () {
     return false;
   });
   $(".minus").click(function () {
-    var pwd_length = $("#pwd_length").val();
+    var pwd_length = correct_pwd_length($("#pwd_length").val());
     var checked = $("#specs").prop("checked");
     this.parentNode.querySelector("input[type=number]").stepDown();
     if (!checked) {
@@ -73,14 +51,14 @@ $(document).ready(function () {
     }
     return false;
   });
-    /*
+   /*
    * FINISH Обработка событий при нажатии увеличения и уменшения значений пароля
    */
   return false;
 });
 
 $("#specs").change(function () {
-  var pwd_length = $("#pwd_length").val();
+  var pwd_length = correct_pwd_length($("#pwd_length").val());
   var checked = $("#specs").prop("checked");
 
   if (!checked) {
@@ -93,6 +71,33 @@ $("#specs").change(function () {
 });
 
 
+  /*
+   * Приведение длины пароля в диапазон от 8 до 20 единиц
+   */
+
+function correct_pwd_length(user_entrance_length) {
+    if (user_entrance_length > 20) {
+      console.log('user_ent_20', user_entrance_length)
+      var pwd_length = 20
+      $("#pwd_length").val(pwd_length)
+      $(".pwd_hint").text("Максимальная длина пароля - 20 символов");
+    }
+    else if (user_entrance_length < 8) {
+      console.log('user_ent_8', user_entrance_length)
+      var pwd_length = 8
+      $("#pwd_length").val(pwd_length)
+      $(".pwd_hint").text("Минимальная длина пароля - 8 символов");
+    }
+    else {
+      var pwd_length = user_entrance_length
+      $(".pwd_hint").text("");
+    }
+    return pwd_length
+};
+
+  /*
+   * AJAX-запрос на сервер
+   */
 
 function makeAjaxRequest(url) {
   $.ajax({
@@ -110,3 +115,5 @@ $(".img_copy").on("click", function () {
   var pwd = $(".show_gen_pswd").html();
   navigator.clipboard.writeText(pwd);
 });
+
+
